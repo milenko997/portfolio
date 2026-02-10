af818ba54bdda668457a1fc80fbc22053b37500c
Milenko Ilic <milenko.ilic997@gmail.com>
2026-02-10 18:12:56 +0100

    Adding components into homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,20 @@
 import './globals.scss';
+import Header from './components/siteHeader';
 import Hero from './components/hero';
+import Projects from './components/projects';
+import About from './components/about';
+import Contact from './components/contact';
+import Footer from './components/siteFooter';
 
 export default function Home() {
   return (
     <main className="main">
+      <Header />
       <Hero />
+      <Projects />
+      <About />
+      <Contact />
+      <Footer />
     </main>
   );
 }
