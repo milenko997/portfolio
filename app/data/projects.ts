f3a4033051c727a3e5d69ac12aa2ef3e5ee2512b
Milenko Ilic <milenko.ilic997@gmail.com>
2026-04-07 16:37:57 +0200

    Updating projects

--- app/data/projects.ts
+++ app/data/projects.ts
@@ -1,10 +1,12 @@
 import type { StaticImageData } from 'next/image';
-import project1 from '@/assets/images/nourishmates.png';
-import project2 from '@/assets/images/strn.png';
-import project3 from '@/assets/images/mpka.png';
-import project4 from '@/assets/images/flameguard.png';
-import project5 from '@/assets/images/framia.png';
-import project6 from '@/assets/images/decem.png';
+import nourishmates from '@/assets/images/nourishmates-new.png';
+import strn from '@/assets/images/strn.png';
+import mpka from '@/assets/images/mpka.png';
+import flameguard from '@/assets/images/flameguard.png';
+import framia from '@/assets/images/framia.png';
+import decem from '@/assets/images/decem.png';
+import milionxl from '@/assets/images/milionxl.png';
+import popgor from '@/assets/images/popgor.png';
 
 export type Project = {
   id: number;
@@ -17,42 +19,56 @@ export type Project = {
 export const projects: Project[] = [
   {
     id: 1,
-    img: project1,
+    img: nourishmates,
     title: 'Nourishmates',
     tech: ['Laravel', 'Svelte', 'MySql', 'REST API'],
     link: 'https://nourishmates.com/',
   },
   {
     id: 2,
-    img: project2,
+    img: strn,
     title: 'STRN',
     tech: ['NEXT.js', 'React', 'Tailwind CSS', 'API Integration'],
     link: 'https://strn-dev.com/',
   },
   {
     id: 3,
-    img: project3,
+    img: milionxl,
+    title: 'Caffe Milion XL',
+    tech: ['NEXT.js', 'React', 'SCSS'],
+    link: 'https://milionxl.rs/',
+  },
+  {
+    id: 4,
+    img: popgor,
+    title: 'PopGor',
+    tech: ['NEXT.js', 'React', 'SCSS'],
+    link: 'https://popgor.rs/',
+  },
+  {
+    id: 5,
+    img: mpka,
     title: 'MPKA',
     tech: ['WordPress', 'PHP', 'SCSS', 'Beaver Builder'],
     link: 'https://mobiler-pflegedienst-karlsruhe.de/',
   },
   {
-    id: 4,
-    img: project4,
+    id: 6,
+    img: flameguard,
     title: 'Flameguard',
     tech: ['WordPress', 'PHP', 'WooCommerce'],
     link: 'https://flameguard.se/',
   },
   {
-    id: 5,
-    img: project5,
+    id: 7,
+    img: framia,
     title: 'Framia',
     tech: ['WordPress', 'PHP', 'WooCommerce', 'Beaver Builder'],
     link: 'https://framia.se/',
   },
   {
-    id: 6,
-    img: project6,
+    id: 8,
+    img: decem,
     title: 'Decem Development',
     tech: ['WordPress', 'PHP', 'MySql', 'SCSS'],
     link: 'https://www.decem.co/',
